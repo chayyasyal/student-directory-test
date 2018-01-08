92f7af9f3f684b566576464bb8ea2393c7aa285c
Chayya_Syal <chayya.syal@bbc.cuk>
2018-01-08 15:16:06 +0000

    Refactored code to make it more readable; separated each process

--- directory.rb
+++ directory.rb
@@ -1,53 +1,55 @@
-def interactive_menu
-  students = []
-  loop do
-    # 1. print the menu and ask user what to do
-    puts '1. Import students'
-    puts '2. Show students'
-    puts '9. Exit'
-    # 2. read input and save it into a variable
-    selection = gets.chomp
-    # 3. do what user has asked
-    case selection
-    when '1'
-      students = input_students
-    when '2'
-      print_header
-      print(students)
-      print_footer(students)
-    when '9'
-      # exit menu
-    else
-      puts 'Computer says no. Try again'
-      # 4. repeat from step 1
-    end
-  end
-end
+@students = []
 
 def input_students
   puts 'Please enter the names of the students'
   puts 'To finish, hit the return key twice'
-  students = []
-  # get first name
   name = gets.chomp
-  # while name not emply, repeate this code
   until name.empty?
-    # add student hash to array
-    students << { name: name, cohort: :November }
-    puts "Now we have #{students.count} students"
-    # get another user name
+    @students << { name: name, cohort: :November }
+    puts "Now we have #{@students.count} students"
     name = gets.chomp
   end
-  # return array of students
+end
+
+def interactive_menu
+  loop do
+    print_menu
+    process(gets.chomp)
+  end
+end
+
+def print_menu
+  puts '1. Input students'
+  puts '2. Show students'
+  puts '9. Exit'
+  end
+
+def show_students
+  print_header
+  print_students_list
+  print_footer
+end
+
+def process(selection)
+  case selection
+  when '1'
+    input_students
+  when '2'
+    show_students
+  when '9'
+    exit
+  else
+    puts 'Computer says no. Try again.'
+  end
 end
 
 def print_header
   puts 'The students of Test Academy'
   puts '--------------------'
-end
+  end
 
-def print(students)
-  students.each do |student|
+def print_students_list
+  @students.each do |student|
     puts "#{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
@@ -57,7 +59,3 @@ def print_footer(students)
 end
 
 interactive_menu
-students = input_students
-print_header
-print(students)
-print_footer(students)
