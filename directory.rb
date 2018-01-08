f1b6762d1a8673bf47068a2db9c3313f34977f54
Chayya_Syal <chayya.syal@bbc.cuk>
2018-01-08 14:30:19 +0000

    Added a method to current iteration which allows the array of students to be more interactive. Works in terminal

--- directory.rb
+++ directory.rb
@@ -1,11 +1,28 @@
-# create an array to list students in
-students = [
-  { name: 'Chayya Syal', cohort: :November },
-  { name: 'Darth Vader', cohort: :November },
-  { name: 'Batman Batman', cohort: :November },
-  { name: 'The Wicked Witch of the West', cohort: :November },
-  { name: 'Covfefe', cohort: :November }
-]
+def input_students
+  puts 'Please enter the names of the students'
+  puts 'To finish, hit the return key twice'
+  students = []
+  # get first name
+  name = gets.chomp
+  # while name not emply, repeate this code
+  until name.empty?
+    # add student hash to array
+    students << { name: name, cohort: :November }
+    puts "Now we have #{students.count} students"
+    # get another user name
+    name = gets.chomp
+  end
+  # return array of students
+end
+
+# # create an array to list students in
+# students = [
+#   { name: 'Chayya Syal', cohort: :November },
+#   { name: 'Darth Vader', cohort: :November },
+#   { name: 'Batman Batman', cohort: :November },
+#   { name: 'The Wicked Witch of the West', cohort: :November },
+#   { name: 'Covfefe', cohort: :November }
+# ]
 def print_header
   puts 'The students of Test Academy'
   puts '--------------------'
@@ -17,10 +34,12 @@ def print(students)
   end
 end
 
-def print_footer(names)
-  puts "Overall, we have #{names.count} great students"
+def print_footer(students)
+  puts "Overall, we have #{students.count} great students"
 end
 
+students = input_students
+# nothing happens until methods are called
 print_header
 print(students)
 print_footer(students)
