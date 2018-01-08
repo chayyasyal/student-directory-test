b8095f056afd3eb0a33d52061716b46eeed57aac
Chayya_Syal <chayya.syal@bbc.cuk>
2018-01-08 14:03:37 +0000

    Part 2 of refactoring completed. Itiration of code in array: it works in terminal

--- directory.rb
+++ directory.rb
@@ -10,10 +10,8 @@ students = [
 # print them like this
 puts 'The students of Test Academy'
 puts '--------------------'
-puts students[0]
-puts students[1]
-puts students[2]
-puts students[3]
-puts students[4]
+students.each do |student|
+  puts student
+end
 # print total number of students here
 puts "Overall, we have #{students.count} great students"
