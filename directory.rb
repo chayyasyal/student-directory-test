73485f6d1042ad8d5b93066bbaa7f202f5986b6b
Chayya_Syal <chayya.syal@bbc.cuk>
2018-01-08 13:58:31 +0000

    Added an array to list of students; works in terminal

--- directory.rb
+++ directory.rb
@@ -1,11 +1,20 @@
 student_count = 5
-# print list of students
+# create an array to list students in
+students = [
+  'Chayya Syal',
+  'Darth Vader',
+  'Batman Batman',
+  'The Wicked Witch of the West',
+  'Covfefe'
+]
+
+# print them like this
 puts 'The students of Test Academy'
-puts '-------------------'
-puts 'Chayya Syal'
-puts 'Darth Vader'
-puts 'Batman Batman'
-puts 'The Wicked Witch of the West'
-puts 'Covfefe'
-# print total number of students
+puts '--------------------'
+puts students[0]
+puts students[1]
+puts students[2]
+puts students[3]
+puts students[4]
+# print total number of students here
 puts "Overall, we have #{student_count} great students"
