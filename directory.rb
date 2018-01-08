7b6a7172d61fbea2ce5175cb8c08689fe249eda7
Chayya_Syal <chayya.syal@bbc.cuk>
2018-01-08 14:10:57 +0000

    Added in methods which extract code and give better structure to program so it reads easier

--- directory.rb
+++ directory.rb
@@ -6,12 +6,21 @@ students = [
   'The Wicked Witch of the West',
   'Covfefe'
 ]
-
-# print them like this
-puts 'The students of Test Academy'
-puts '--------------------'
-students.each do |student|
-  puts student
+def print_header
+  puts 'The students of Test Academy'
+  puts '--------------------'
 end
-# print total number of students here
-puts "Overall, we have #{students.count} great students"
+
+def print(names)
+  names.each do |name|
+    puts name
+  end
+end
+
+def print_footer(names)
+  puts "Overall, we have #{names.count} great students"
+end
+
+print_header
+print(students)
+print_footer(students)
