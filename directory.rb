4875dee86827ff91e5c0de7c50e08d4697e87499
Chayya_Syal <chayya.syal@bbc.cuk>
2018-01-08 13:29:44 +0000

    Added comment

--- directory.rb
+++ directory.rb
@@ -5,7 +5,7 @@ puts 'Darth Vader'
 puts 'Batman Batman'
 puts 'The Wicked Witch of the West'
 puts 'Covfefe'
-
+# print total number of students
 print 'Overall, we have '
 print 5
 puts ' great students'
