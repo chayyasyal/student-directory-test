73eb8df04f32f57a44d71c2535046d500c824fb2
Chayya_Syal <chayya.syal@bbc.cuk>
2018-01-09 12:04:04 +0000

    Changed flow of commands, edited syntax on how students appear when directory is displayed. Successfully works in terminal: have created a fully functional command line app git push

--- directory.rb
+++ directory.rb
@@ -1,52 +1,23 @@
-@students =
-  ['name' => 'Chayya Syal', 'cohort' => 'November']
-['name' => 'Darth Vader', 'cohort' => 'November']
-['name' => 'Batman Batman', 'cohort' => 'November']
-['name' => 'The Wicked Witch of the West', 'cohort' => 'November']
-['name' => 'Covfefe', 'cohort' => 'November']
+require 'csv'
 
-def input_students
-  puts 'Please enter the names of the students'
-  puts 'To finish, hit the return key twice'
-  name = gets.chomp
-  until name.empty?
-    @students << { name: name, cohort: :November }
-    puts "Now we have #{@students.count} students"
-    name = gets.chomp
-  end
-end
+@students = []
 
 def interactive_menu
   loop do
     print_menu
-    process(gets.chomp)
+    process(STDIN.gets.chomp)
   end
 end
 
 def print_menu
   puts '1. Input the students'
-  puts '2. Show the students'
+  puts '2. Show the student directory'
   puts '3. Save list to students.csv'
   puts '4. Load the list from students.csv'
   puts '9. Exit'
 
     # will add more items to menu
   end
-
-def show_students
-  print_header
-  print_students_list
-  print_footer
-end
-
-def load_students
-  file = File.open('students.csv', 'r')
-  file.readlines.each do |line|
-    name, cohort = line.chomp.split(',')
-    @students << { name: name, cohort: cohort.to_sym }
-  end
-  file.close
-end
 
 def process(selection)
   case selection
@@ -65,14 +36,52 @@ def process(selection)
   end
 end
 
+def input_students
+  puts 'Please enter the names of the students'
+  puts 'To finish, hit the return key twice'
+  name = STDIN.gets.chomp
+  until name.empty?
+    @students << { name: name, cohort: :November }
+    puts "Now we have #{@students.count} students"
+    name = STDIN.gets.chomp
+  end
+end
+
+def show_students
+  print_header
+  print_students_list
+  print_footer
+end
+
 def save_students
   file = File.open('students.csv', 'w')
   @students.each do |student|
     student_data = [student[:name], student[:cohort]]
     csv_line = student_data.join(',')
-    file.puts 'This is written to a file'
+    file.puts csv_line
   end
   file.close
+end
+
+def load_students
+  file = File.open(filename = 'students.csv', 'r')
+  file.readlines.each do |line|
+    name, cohort = line.chomp.split(',')
+    @students << { name: name, cohort: cohort }
+  end
+  file.close
+end
+
+def try_load_students
+  filename = ARGV.first
+  return if filename.nil?
+  if File.exist?
+    load_students(filename)
+    puts "Loaded #{@students.count} from students.csv"
+  else
+    puts "Sorry #{filename} doesn't seem to exist. Please try again."
+    exit
+  end
 end
 
 def print_header
@@ -82,7 +91,7 @@ def print_header
 
 def print_students_list
   @students.each do |student|
-    puts "#{student[:name]} (#{student[:cohort]} cohort)"
+    puts "#{student[:name]} (#{student[:cohort]})"
   end
 end
 
@@ -90,4 +99,6 @@ def print_footer
   puts "Overall, we have #{@students.count} great students"
 end
 
+load_students
+try_load_students
 interactive_menu
