b11b9abfeda6c46faebb243d45d42b47a169da12
Chayya_Syal <chayya.syal@bbc.cuk>
2018-01-09 11:11:57 +0000

    Added method to load data from file

--- directory.rb
+++ directory.rb
@@ -27,6 +27,7 @@ def print_menu
   puts '1. Input the students'
   puts '2. Show the students'
   puts '3. Save list to students.csv'
+  puts '4. Load the list from students.csv'
   puts '9. Exit'
 
     # will add more items to menu
@@ -38,6 +39,15 @@ def show_students
   print_footer
 end
 
+def load_students
+  file = File.open('students.csv', 'r')
+  file.readlines.each do |line|
+    name, cohort = line.chomp.split(',')
+    @students << { name: name, cohort: cohort.to_sym }
+  end
+  file.close
+end
+
 def process(selection)
   case selection
   when '1'
@@ -46,6 +56,8 @@ def process(selection)
     show_students
   when '3'
     save_students
+  when '4'
+    load_students
   when '9'
     exit
   else
